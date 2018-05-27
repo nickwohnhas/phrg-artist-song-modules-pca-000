b87f823f08306efe071c92b2301714446503f7ab
Nick Wohnhas <nicholas.wohnhas@powerhrg.com>
2018-05-27 12:56:41 -0400

    Practice with Modules

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,18 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def initialize
     @@artists << self
@@ -17,14 +21,6 @@ class Artist
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
   end
 
   def add_song(song)
